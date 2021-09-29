6e7f8cd5b2437ac2a70de3531fec0ed8d105476e
MohanramAnbukkarasu <44049406+MohanramAnbukkarasu@users.noreply.github.com>
2021-09-29 19:04:33 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Win32;
+using Syncfusion.Data;
+using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.UI.Xaml.Grid.Converter;
+using Syncfusion.UI.Xaml.Grid.Helpers;
+using Syncfusion.UI.Xaml.TreeGrid;
+using Syncfusion.XlsIO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SfDataGrid_MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var options = new ExcelExportingOptions();
+            options.ExcelVersion = ExcelVersion.Excel2016;
+            var excelEngine = new CustomSfDataGridToExcelConverter().ExportToExcel(this.dataGrid, dataGrid.View, options);
+
+            var workBook = excelEngine.Excel.Workbooks[0];
+            
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                FilterIndex = 2,
+                Filter = "Excel 97 to 2003 Files(*.xls)|*.xls|Excel 2007 to 2010 Files(*.xlsx)|*.xlsx|Excel 2013 File(*.xlsx)|*.xlsx"
+            };
+
+            if (sfd.ShowDialog() == true)
+            {
+                using (Stream stream = sfd.OpenFile())
+                {
+
+                    if (sfd.FilterIndex == 1)
+                        workBook.Version = ExcelVersion.Excel97to2003;
+
+                    else if (sfd.FilterIndex == 2)
+                        workBook.Version = ExcelVersion.Excel2010;
+
+                    else
+                        workBook.Version = ExcelVersion.Excel2013;
+                    workBook.SaveAs(stream);
+                }
+
+                //Message box confirmation to view the created workbook.
+
+                if (MessageBox.Show("Do you want to view the workbook?", "Workbook has been created",
+                                    MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                {
+
+                    //Launching the Excel file using the default Application.[MS Excel Or Free ExcelViewer]
+                    System.Diagnostics.Process.Start(sfd.FileName);
+                }
+            }
+        }
+    }
+}
